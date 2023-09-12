fa89a31f1bc95b4ac51d37fe89f9da8612dab047
Kazuki Minakawa <kazuki.minakawa@funlab.jp>
2023-09-12 04:40:12 +0000

    seed修正

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,13 +2,29 @@ import { hashPassword } from '../src/helpers/password'
 import { prisma } from '../src/helpers/prisma'
 
 async function main() {
-  await prisma.user.create({
-    data: {
-      name: 'admin',
-      passwordHash: hashPassword('lab@user00'),
-      isAdmin: true,
-    },
-  })
+  const admin = await prisma.user.findUnique({ where: { name: 'admin' } })
+  if (!admin) {
+    console.log('create admin')
+    await prisma.user.create({
+      data: {
+        name: 'admin',
+        passwordHash: hashPassword('lab@user00'),
+        isAdmin: true,
+      },
+    })
+  }
+
+  const testuser = await prisma.user.findUnique({ where: { name: 'testuser' } })
+  if (!testuser) {
+    console.log('create testuser')
+    await prisma.user.create({
+      data: {
+        name: 'testuser',
+        passwordHash: hashPassword('lab@user00'),
+        isAdmin: false,
+      },
+    })
+  }
 }
 
 main()
