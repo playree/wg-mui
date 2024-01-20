c66857d20a5a3fe6a6431effc86d5e6128cbe043
Kazuki Minakawa <kazuki.minakawa@funlab.jp>
2024-01-20 16:24:50 +0000

    Initialize処理追加

--- src/app/initialize/server-actions.ts
+++ src/app/initialize/server-actions.ts
@@ -2,13 +2,25 @@
 
 import { prisma } from '@/helpers/prisma'
 import { InitializeWgConf } from '@/helpers/schema'
-import { genPublicKey } from '@/server-actions/cmd'
+import { createWgConfDir, genPublicKey } from '@/server-actions/cmd'
 
 export const initializeWgConf = async (conf: InitializeWgConf) => {
-  console.debug('initializeWgConf:')
+  console.info('initializeWgConf:')
+
+  // 公開鍵生成
   const publicKey = await genPublicKey(conf.privateKey)
   if (!publicKey) {
     throw new Error('Failed to generate public key')
   }
+  console.info('genPublicKey: ok')
+
+  // 設定ディレクトリ作成
+  if (!createWgConfDir(conf.confDirPath)) {
+    throw new Error('Failed to create conf directory')
+  }
+  console.info('createWgConfDir: ok')
+
+  // DB保存
   await prisma.wgConf.create({ data: { ...conf, publicKey } })
+  console.info('save WgConf: ok')
 }
--- src/server-actions/cmd.ts
+++ src/server-actions/cmd.ts
@@ -1,7 +1,13 @@
 'use server'
 
 import { ExecException, exec } from 'child_process'
+import path from 'path'
 
+/**
+ * Shell Command 実行
+ * @param cmd
+ * @returns
+ */
 export const runCmd = async (cmd: string) => {
   return new Promise<{
     error: ExecException | null
@@ -18,6 +24,10 @@ export const runCmd = async (cmd: string) => {
   })
 }
 
+/**
+ * WireGuard Version取得
+ * @returns
+ */
 export const getWgVersion = async () => {
   const res = await runCmd('sudo wg -v')
   if (res.error) {
@@ -26,6 +36,10 @@ export const getWgVersion = async () => {
   return res.error ? undefined : res.stdout.split(' - ')[0]
 }
 
+/**
+ *
+ * @returns IP Forward設定取得
+ */
 export const getIpForward = async () => {
   const res = await runCmd('sudo sysctl net.ipv4.ip_forward')
   if (res.error) {
@@ -34,6 +48,10 @@ export const getIpForward = async () => {
   return res.error ? undefined : res.stdout.replace(/\r?\n/g, '')
 }
 
+/**
+ * WireGuard秘密鍵生成
+ * @returns
+ */
 export const genPrivateKey = async () => {
   const res = await runCmd(`wg genkey`)
   if (res.error) {
@@ -42,10 +60,29 @@ export const genPrivateKey = async () => {
   return res.error ? undefined : res.stdout.replace(/\r?\n/g, '')
 }
 
+/**
+ * WireGuard公開鍵生成
+ * @param privateKey
+ * @returns
+ */
 export const genPublicKey = async (privateKey: string) => {
   const res = await runCmd(`echo ${privateKey} | wg pubkey`)
   if (res.error) {
     console.debug('genPublicKey:', res.stderr)
   }
   return res.error ? undefined : res.stdout.replace(/\r?\n/g, '')
+}
+
+/**
+ * 設定ディレクトリ作成
+ * @param dirPath
+ * @returns
+ */
+export const createWgConfDir = async (dirPath: string) => {
+  const peerDir = path.join(dirPath, 'peer')
+  const res = await runCmd(`sudo mkdir -p ${peerDir}`)
+  if (res.error) {
+    console.debug('createWgConfDir:', res.stderr)
+  }
+  return !res.error
 }
