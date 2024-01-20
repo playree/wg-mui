2322f5f23adc628126d6e697b72e74d3c6f222fd
Kazuki Minakawa <kazuki.minakawa@funlab.jp>
2024-01-20 09:34:51 +0000

    サインイン画面リファクタリング

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,16 +1,17 @@
 'use client'
 
 import { ArrowLeftOnRectangleIcon, EyeIcon, EyeSlashIcon } from '@/components/icons'
+import { InputCtrl } from '@/components/nextekit/ui/input'
 import { Message } from '@/components/nextekit/ui/message'
 import { gridStyles } from '@/components/styles'
 import { Signin, scSignin } from '@/helpers/schema'
 import { useLocale } from '@/locale'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Button, Card, CardBody, CardHeader, Input } from '@nextui-org/react'
+import { Button, Card, CardBody, CardHeader } from '@nextui-org/react'
 import { signIn } from 'next-auth/react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { FC, useState } from 'react'
-import { Controller, SubmitHandler, useForm } from 'react-hook-form'
+import { SubmitHandler, useForm } from 'react-hook-form'
 
 const SignIn: FC = () => {
   const { t, fet } = useLocale()
@@ -26,7 +27,11 @@ const SignIn: FC = () => {
     control,
     handleSubmit,
     formState: { errors },
-  } = useForm<Signin>({ resolver: zodResolver(scSignin), mode: 'onChange' })
+  } = useForm<Signin>({
+    resolver: zodResolver(scSignin),
+    mode: 'onChange',
+    defaultValues: { username: '', password: '' },
+  })
 
   const onSubmit: SubmitHandler<Signin> = async (data) => {
     console.debug('SignIn:submit:', data)
@@ -54,47 +59,32 @@ const SignIn: FC = () => {
         {isAuthNg && <Message variant='error'>{t('@invalid_username_or_password')}</Message>}
         <form className={gridStyles()} onSubmit={handleSubmit(onSubmit)}>
           <div className='col-span-12 p-2'>
-            <Controller
+            <InputCtrl
               control={control}
               name='username'
-              render={({ field: { onChange, value } }) => (
-                <Input
-                  autoFocus
-                  type='text'
-                  label={t('item_username')}
-                  variant='bordered'
-                  autoComplete='username'
-                  errorMessage={fet(errors.username)}
-                  onChange={onChange}
-                  value={value}
-                />
-              )}
+              autoFocus
+              label={t('item_username')}
+              autoComplete='username'
+              errorMessage={fet(errors.username)}
             />
           </div>
           <div className='col-span-12 p-2'>
-            <Controller
+            <InputCtrl
               control={control}
               name='password'
-              render={({ field: { onChange, value } }) => (
-                <Input
-                  label={t('item_password')}
-                  variant='bordered'
-                  endContent={
-                    <button className='focus:outline-none' type='button' onClick={toggleVisibility}>
-                      {isVisible ? (
-                        <EyeSlashIcon className='pointer-events-none text-2xl text-default-400' />
-                      ) : (
-                        <EyeIcon className='pointer-events-none text-2xl text-default-400' />
-                      )}
-                    </button>
-                  }
-                  type={isVisible ? 'text' : 'password'}
-                  autoComplete='current-password'
-                  errorMessage={fet(errors.password)}
-                  onChange={onChange}
-                  value={value}
-                />
-              )}
+              label={t('item_password')}
+              endContent={
+                <button className='focus:outline-none' type='button' onClick={toggleVisibility}>
+                  {isVisible ? (
+                    <EyeSlashIcon className='pointer-events-none text-2xl text-default-400' />
+                  ) : (
+                    <EyeIcon className='pointer-events-none text-2xl text-default-400' />
+                  )}
+                </button>
+              }
+              type={isVisible ? 'text' : 'password'}
+              autoComplete='current-password'
+              errorMessage={fet(errors.password)}
             />
           </div>
           <div className='col-span-12 p-2 text-center'>
