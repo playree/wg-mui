3e513b848e5679395d2b720cf1880a7701c71271
Kazuki Minakawa <kazuki.minakawa@funlab.jp>
2024-07-18 12:02:03 +0000

    メール送信機能をsendmailとSMTPに対応

--- src/helpers/mail.ts
+++ src/helpers/mail.ts
@@ -1,17 +1,22 @@
 import { t } from '@/locale/server'
 import sgMail from '@sendgrid/mail'
+import { createTransport } from 'nodemailer'
 
 import { getAppName } from './env'
 import { errSystemError } from './error'
 import { TypeUser } from './schema'
 
 type SendEmail = {
-  to: string | { name?: string; email: string }
-  from: string | { name?: string; email: string }
+  to: string | { name: string; email: string; address: string }
+  from: string | { name: string; email: string; address: string }
   subject: string
   text: string
 }
 
+/**
+ * SendGrid
+ * @param param
+ */
 const sendGrid = async (param: SendEmail) => {
   if (!process.env.SENDGRID_API_KEY) {
     throw errSystemError('SENDGRID_API_KEY not set')
@@ -21,10 +26,57 @@ const sendGrid = async (param: SendEmail) => {
   await sgMail.send(param)
 }
 
+/**
+ * sendmailコマンド
+ * @param param
+ */
+const sendMail = async (param: SendEmail) => {
+  if (!process.env.SENDMAIL_PATH) {
+    throw errSystemError('SENDMAIL_PATH not set')
+  }
+
+  const tp = createTransport({
+    sendmail: true,
+    newline: 'unix',
+    path: process.env.SENDMAIL_PATH,
+  })
+  await tp.sendMail(param)
+}
+
+/**
+ * SMTP
+ * @param param
+ */
+const sendSmtp = async (param: SendEmail) => {
+  if (!process.env.SMTP_HOST) {
+    throw errSystemError('SMTP_HOST not set')
+  }
+  if (!process.env.SMTP_PORT) {
+    throw errSystemError('SMTP_PORT not set')
+  }
+  if (!process.env.SMTP_USER) {
+    throw errSystemError('SMTP_USER not set')
+  }
+  if (!process.env.SMTP_PASS) {
+    throw errSystemError('SMTP_PASS not set')
+  }
+
+  const tp = createTransport({
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    secure: true,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  })
+  await tp.sendMail(param)
+}
+
 const sendEmail = async (param: Omit<SendEmail, 'from'>) => {
   console.debug('sendEmail:', param.subject, param.to)
   if (!process.env.MAIL_FROM) {
-    throw errSystemError('SENDGRID_API_KEY not set')
+    throw errSystemError('MAIL_FROM not set')
   }
 
   if (process.env.DEBUG_SEND_EMAIL) {
@@ -36,6 +88,7 @@ const sendEmail = async (param: Omit<SendEmail, 'from'>) => {
   const from = {
     name: getAppName(),
     email: process.env.MAIL_FROM,
+    address: process.env.MAIL_FROM,
   }
   switch (process.env.MAIL_SEND) {
     case 'sendgrid':
@@ -43,8 +96,18 @@ const sendEmail = async (param: Omit<SendEmail, 'from'>) => {
         from,
         ...param,
       })
+    case 'sendmail':
+      return sendMail({
+        from,
+        ...param,
+      })
+    case 'smtp':
+      return sendSmtp({
+        from,
+        ...param,
+      })
   }
-  throw errSystemError('MAIL_SEND not set')
+  throw errSystemError('Unable to send email')
 }
 
 /**
@@ -53,9 +116,6 @@ const sendEmail = async (param: Omit<SendEmail, 'from'>) => {
  * @param onetimeId
  */
 export const sendEmailPasswordReset = async (param: { user: TypeUser; to: string; onetimeId: string }) => {
-  if (!process.env.SENDGRID_API_KEY) {
-    throw errSystemError('SENDGRID_API_KEY not set')
-  }
   const { user, to, onetimeId } = param
 
   const url = new URL(`/pwreset/${onetimeId}`, process.env.NEXTAUTH_URL)
@@ -74,9 +134,6 @@ export const sendEmailPasswordReset = async (param: { user: TypeUser; to: string
  * @param onetimeId
  */
 export const sendEmailConfirm = async (param: { username: string; locale?: string; to: string; onetimeId: string }) => {
-  if (!process.env.SENDGRID_API_KEY) {
-    throw errSystemError('SENDGRID_API_KEY not set')
-  }
   const { username, locale = '', to, onetimeId } = param
 
   const url = new URL(`/mlchange/${onetimeId}`, process.env.NEXTAUTH_URL)
